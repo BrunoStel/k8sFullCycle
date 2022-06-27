f57572e2d40f75a3d78e7e8091a4d9df9612dec7
Bruno Stelmastchuk <brunostel@gmail.com>
2022-06-27 09:44:03 -0300

    Add healthz endpoint and livenessProbe to k8s

--- server.go
+++ server.go
@@ -42,12 +42,12 @@ func Secret(w http.ResponseWriter, r *http.Request) {
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	duration := time.Since(startedAt)
 
-	if duration.Seconds() > 25 {
+	if duration.Seconds() < 10 {
 		w.WriteHeader(500)
 		w.Write([]byte(fmt.Sprint("Durantion: %v", duration.Seconds())))
 	}else {
 		w.WriteHeader(200)
 		w.Write([]byte(fmt.Sprint("Ok")))
 	}
-	
+
 }
